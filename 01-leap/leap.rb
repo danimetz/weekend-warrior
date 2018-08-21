ba2418a8f369302026e992f6b780057dd596c609
Danielle Metzner <danimetzner@gmail.com>
2018-08-21 12:33:27 -0700

    Leap year with addtional solutions

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -3,12 +3,20 @@
 #   except every year that is evenly divisible by 100
 #     unless the year is also evenly divisible by 400
 
+# def leap_year?(year)
+#   if year % 4 == 0
+#     if year % 100 == 0 && year % 400 != 0
+#      return false
+#     else
+#      return true
+#    end
+#  end
+# end
+
+# def leap_year?(year)
+#   year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+# end
+
 def leap_year?(year)
-  if year % 4 == 0
-    if year % 100 == 0 && year % 400 != 0
-     return false
-    else
-     return true
-   end
- end
+  year % 400 == 0 || year % 4 == 0 && year % 100 != 0
 end
