dadf0727b065d1d78ef6707e6a302d8f8f9493c0
Danielle Metzner <danimetzner@gmail.com>
2018-09-16 15:47:41 -0700

    Word Count passes tests

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,9 @@
+def words(words)
+  words = words.split(" ")
 
+  word_hash = {}
+  words.each do |word|
+    word_hash[word] = words.count{ |word_check| word_check==word }
+  end
+  return word_hash
+end
