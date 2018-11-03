50810f9998071ff8dbcf6c57eb8eccbe43ef71d3
Danielle Metzner <danimetzner@gmail.com>
2018-11-03 14:00:40 -0700

    Added comments and refactored the code

--- 07-roman-numeral-reverter/roman_reverter.rb
+++ 07-roman-numeral-reverter/roman_reverter.rb
@@ -9,31 +9,32 @@ class Roman
     rank_letters = ["M", "D", "C", "L", "X", "V", "I"]
 
     sum = 0
+    i = 0
 
-    if roman.length == 1
-      sum += rank[roman.to_sym]
-    else
-      i = 0
-      while i < roman.length
-        letter = roman[i]
-        higher_index = rank_letters.index(letter)
-        
-        if roman[i+1].nil?
-          sum += rank[letter.to_sym]
-          i += 1
-        elsif higher_index > rank_letters.index(roman[i+1])
-          sum += (rank[roman[i+1].to_sym] - rank[letter.to_sym])
-          i += 2
-        else
-          sum += rank[letter.to_sym]
-          i += 1
-          # binding.pry
-        end
+    #I want to loop through every index of the roman numeral given
+    while i < roman.length
+      letter = roman[i]
+
+      #I will use this to check if the next roman numeral has a higher value
+      higher_index = rank_letters.index(letter)
+
+      #If we are at the last index of the roman numeral just add the current value to the sum
+      if roman[i+1].nil?
+        sum += rank[letter.to_sym]
+        i += 1
+      # If the next index is higher value then we need to subtract the current value of the
+      # roman numeral from the next vlaue of the roman numeral
+      elsif higher_index > rank_letters.index(roman[i+1])
+        sum += (rank[roman[i+1].to_sym] - rank[letter.to_sym])
+        i += 2
+      #Otherwise just add the value of the roman numeral
+      else
+        sum += rank[letter.to_sym]
+        i += 1
       end
     end
     return sum
   end
 
-end
 
-puts Roman.reverter("XXX")
+end
